711533307a700c1ff51477ab41eebd937fd29675
Cluyverth <ContatoCluyverth@gmail.com>
2022-01-29 16:35:57 -0300

    Light Sensor Update, Potentiometer Controls and Buttons Logic

--- Robotic-Arm-Code/Black-Arm.ino
+++ Robotic-Arm-Code/Black-Arm.ino
@@ -43,17 +43,46 @@ void setup() {
 //== LOOP BEGINNING
 void loop() { 
 //== LOOP VARIABLES
-    int ldrValue = analogRead(pinLDR);
-// ldrValue = map(ldrValue,0,1023,0,255); //! if you want convert from analog to digital
+    int ldrValue = analogRead(pinLDR); //* read value from sensor
+    // ldrValue = map(ldrValue,0,1023,0,255); //! if you want convert from analog to digital
+    Serial.println(analogRead(pinLDR)); //* print value from sensor
+    int Butt = digitalRead(butt); //* read the button value
+    int Butt2 = digitalRead(butt2); //* read the button value
 //== END LOOP VARIABLES
+
+//== POTENTIOMETER CONTROLS //! use to see the limits of the arm setup you created before configuring the arm
+//motor1 = map(analogRead(pot1),0,1023,0,180); //* convert the analog potentiometer to angles
+//motor2 = map(analogRead(pot2),0,1023,0,180); //* convert the analog potentiometer to angles
+//motor3 = map(analogRead(pot3),0,1023,0,180); //* convert the analog potentiometer to angles
+// motor4 = map(analogRead(pot4),0,1023,0,180); //* convert the analog potentiometer to angles
+// serv1.write(motor1); //* do the serv move
+//serv2.write(motor2); //* do the serv move
+//serv3.write(motor3); //* do the serv move
+//serv4.write(motor4); //* do the serv move
+//== END OF POTENTIOMETER CONTROLS
 
 //== LIGHT SENSOR LOGIC
     if(ldrValue > lightLevel){
         digitalWrite(pinLED, HIGH); //? turns on the LED to indicate that the sensor has seen the new cargo truck
         delay(1000);
         digitalWrite(pinLED, LOW);
-    }
 //== END OF LIGHT SENSOR LOGIC
+
+        //== BUTTONS LOGIC
+        if(Butt == LOW){ //* See if the button is pressed
+            digitalWrite(pinLED2, HIGH); //* turn on the LED light to indicate the beginning of the movement of the arm
+            delay(1000);
+            digitalWrite(pinLED2, LOW); //* turn off the LED light to indicate the end of the movement of the arm
+            delay(100);   
+        }
+        if(Butt2 == LOW){ //* See if the button is pressed
+            digitalWrite(pinLED2, HIGH); //* turn on the LED light to indicate the beginning of the movement of the arm
+            delay(1000);
+            digitalWrite(pinLED2, LOW); //* turn off the LED light to indicate the end of the movement of the arm
+            delay(100);   
+        }
+        //== END OF BUTTONS LOGIC
+    }
 
 }
 //== END LOOP

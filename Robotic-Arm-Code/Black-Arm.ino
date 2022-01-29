32a75cdc6911d28919b7e4ff65d7f3c4157f4014
Cluyverth <ContatoCluyverth@gmail.com>
2022-01-29 16:12:01 -0300

    Includes and defines

---
+++ Robotic-Arm-Code/Black-Arm.ino
@@ -0,0 +1,38 @@
+//== INCLUDES
+#include <Servo.h> //* include the library Servo
+#include <SoftwareSerial.h> //* include the library SoftwareSerial
+#include <Arduino.h> //! if you use Arduino IDE you don't need to include this
+//== END INCLUDES
+
+//== DEFINES
+#define pinLDR A0  //* set analog pin 0 as the LDR pin
+#define pot1 A1 //* potentiometer 1
+#define pot2 A2 //* potentiometer 2
+#define pot3 A3 //* potentiometer 3
+#define pot4 A4 //* potentiometer 4
+#define lightLevel 200 //* light level to be calibrated
+#define pinServ1 7 //* set digital pin 7 as servomotor 1 pin
+#define pinServ2 6 //* set digital pin 6 as servomotor 2 pin
+#define pinServ3 5 //* set digital pin 5 as servomotor 3 pin
+#define pinServ4 4 //* set digital pin 4 as servomotor 4 pin
+#define pinLED 3 //* set digital pin 3 as LED 1 pin
+#define pinLED2 2 //* set digital pin 2 as LED 2 pin
+#define butt 8 //* set digital pin 8 as Button 1 pin
+#define but2 9 //* set digital pin 9 as Button 2 pin
+//== END DEFINES
+
+//== SETUP
+void setup() {
+
+}
+//== END SETUP
+
+//== LOOP BEGINNING
+void loop() { 
+
+}
+//== END LOOP
+
+//== AUXILIARIES FUNCTION
+
+//== END AUXILIARIES FUNCTION
